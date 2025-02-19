290a2fa995f323e9f76453234afcb1439ec5cc28
lakshita018 <lakshitagupta0518@gmail.com>
2025-02-19 13:13:38 +0545

    movies

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var words = ["Kapoor and sons","Dil chahta hai","house of secrets","interstellar","laapta ladies"]; // Word list including a phrase
+var words = ["Dil Chahta Hai","Kapoor & Sons","Taare Zameen Par","Gangs of Wasseypur","House of Secrets","House of Cards","The Walking Dead","Alice in Borderland","Kabhi Alvida Naa Kehna","The Vampire Diaries","The Adam Project","The Theory of Everything","Interstellar","Laapataa Ladies","Young Sheldon","Yeh Jawaani Hai Deewani","The Notebook","The Conjuring","The Girl on the Train","Murder Mubarak","Never Have I Ever","Humpty Sharma Ki Dulhania","Chor Nikal Ke Bhaga","Gangubai Kathiawadi","Jurassic World","Madagascar","The Good Doctor","Emily in Paris","The Girl Next Door","Black Warrant","The Railway Men","Gully Boy","Mission Majnu","Bajrangi Bhaijaan","Lucky Bhaskar","Bhool Bhulaiyaa","Kho Gaye Hum Kahan","The Fame Game","Kaala Paani","Death Note","Delhi Crime","Yeh Kaali Kaali Aankhein"
+];
 var buttons = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 var score = 0;
 var tries = [];
